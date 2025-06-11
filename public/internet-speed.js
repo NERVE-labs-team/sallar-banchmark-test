51d30a7f6e7be632bcef02a47736e2d0122a8368
Jakub Padło <jakubpadlo11@gmail.com>
2025-06-11 11:36:11 +0200

    Add internet speed test

---
+++ public/internet-speed.js
@@ -0,0 +1,57 @@
+const runTestCase = async () => {
+  let startTime, endTime;
+  let imageSize = '';
+  let image = new Image();
+  let speedInMbps = null;
+
+  let imageLink =
+    'https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg'; // duży plik
+  let noCacheLink = imageLink + '?nocache=' + Math.random(); // unikamy cache
+
+  image.onload = async function () {
+    endTime = new Date().getTime();
+
+    await fetch(noCacheLink, { cache: 'no-store' }).then((response) => {
+      imageSize = response.headers.get('content-length');
+      calculateSpeed();
+    });
+  };
+
+  const calculateSpeed = () => {
+    let timeDuration = (endTime - startTime) / 1000;
+    let loadedBits = imageSize * 8;
+    let speedInBps = (loadedBits / timeDuration).toFixed(2);
+    let speedInKbps = (speedInBps / 1024).toFixed(2);
+    speedInMbps = (speedInKbps / 1024).toFixed(2);
+  };
+
+  const getResult = async () => {
+    return new Promise((resolve) => {
+      const waitOnResult = () => {
+        if (speedInMbps === null) {
+          setTimeout(waitOnResult, 50);
+        } else resolve(Number(speedInMbps));
+      };
+      waitOnResult();
+    });
+  };
+
+  startTime = new Date().getTime();
+  image.src = noCacheLink;
+
+  return await getResult();
+};
+
+export const runInternetSppedTest = async () => {
+  const ITERATIONS = 20;
+  let sum = 0;
+
+  for (let i = 0; i < ITERATIONS; ++i) {
+    console.log(`Internet speed test #${i + 1}`);
+    const result = await runTestCase();
+    sum += result;
+    await new Promise((r) => setTimeout(r, 100));
+  }
+
+  return sum / ITERATIONS;
+};
--- public/program.js
+++ public/program.js
@@ -1,10 +1,16 @@
+import { runInternetSppedTest } from './internet-speed.js';
+
 const program = new SallarNetworkClient.InstanceManager(io);
 
 program.on('perform-benchmark', async ({ value }, manager) => {
+  const internetSpeed = Math.round(await runInternetSppedTest());
+
+  console.log(`Measured ${internetSpeed} Mb/s download speed`);
+
   // TODO: Perform actual benchmark
   manager.emit('benchmark-finished', {
     cpu_points: 1,
     gpu_points: 1,
-    internet_speed: 1, // Mb/s
+    internet_speed: internetSpeed,
   });
 });
