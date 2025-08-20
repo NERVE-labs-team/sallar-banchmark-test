95e168ba47619c08c33cb59b8ad9fade1b5731fe
Jakub Padło <jakubpadlo11@gmail.com>
2025-08-20 21:36:02 +0200

    Add logs to cpu tests

--- public/benchmarks/cpu-points.js
+++ public/benchmarks/cpu-points.js
@@ -1,9 +1,12 @@
 const cpuHeavyComputation = (iterations) => {
   let total = 0;
+  console.log('Run cpu heavy operation');
   for (let i = 0; i < iterations; i++) {
     const x = Math.sin(i) * Math.cos(i) * Math.tan(i % 360);
     total += x * x + Math.sqrt(Math.abs(x));
   }
+  console.log('End cpu heavy operation');
+
   return total;
 };
 
@@ -25,6 +28,7 @@ export const runCpuTest = () => {
     let sum = 0;
 
     for (let i = 0; i < ITERATIONS; ++i) {
+      console.log(`Cpu test #${i}`);
       const result = runTestCase();
       sum += result;
     }
